550074a7d19109a2c5c987f7900b760f75669f72
sebastian.schmidt <bastman77@gmail.com>
2017-08-13 10:47:08 +0200

    querydsl example (moved from tweeter-domain to realestate-domain)

--- src/main/kotlin/com/example/demo/api/realestate/handler/common/request/queryDsl.kt
+++ src/main/kotlin/com/example/demo/api/realestate/handler/common/request/queryDsl.kt
@@ -1,5 +1,10 @@
 package com.example.demo.api.realestate.handler.common.request
 
+import com.example.demo.api.common.BadRequestException
+import com.example.demo.api.realestate.domain.PropertyType
+import java.time.Instant
+import java.util.*
+
 object QueryDslOperation {
     const val ASC = "asc"
     const val DESC = "desc"
@@ -7,4 +12,52 @@ object QueryDslOperation {
     const val EQ = "eq"
     const val GOE = "goe"
     const val LOE = "loe"
+}
+
+object QueryDslRequestParser {
+    fun asInstant(fieldValue:String, fieldExpression: String): Instant {
+        return try {
+            Instant.parse(fieldValue)
+        } catch (all: Exception) {
+            throw BadRequestException(
+                    "Failed to parse field.value"
+                            + " provided by field.expression=$fieldExpression"
+                            +" as Instant!"
+                            +" reason=${all.message} !"
+                            +" example=$INSTANT_EXAMPLE"
+            )
+        }
+    }
+
+    fun asUUID(fieldValue:String, fieldExpression: String):UUID {
+        return try {
+            UUID.fromString(fieldValue)
+        } catch (all: Exception) {
+            throw BadRequestException(
+                    "Failed to parse field.value"
+                            + " provided by field.expression=$fieldExpression"
+                            +" as UUID!"
+                            +" reason=${all.message} !"
+                            +" example=$UUID_EXAMPLE"
+            )
+        }
+    }
+
+    fun asPropertyType(fieldValue:String, fieldExpression: String):PropertyType {
+        return try {
+            PropertyType.valueOf(fieldValue)
+        } catch (all: Exception) {
+            throw BadRequestException(
+                    "Failed to parse field.value"
+                            + " provided by field.expression=$fieldExpression"
+                            +" as PropertyType!"
+                            +" reason=${all.message} !"
+                            +" examples=$PROPERTY_TYPES_ALLOWED"
+            )
+        }
+    }
+
+    private val INSTANT_EXAMPLE = Instant.ofEpochSecond(1501595115)
+    private val UUID_EXAMPLE = UUID.randomUUID()
+    private val PROPERTY_TYPES_ALLOWED = PropertyType.values()
 }
--- src/main/kotlin/com/example/demo/api/realestate/handler/search/requests.kt
+++ src/main/kotlin/com/example/demo/api/realestate/handler/search/requests.kt
@@ -1,16 +1,36 @@
 package com.example.demo.api.realestate.handler.search
 
+import com.example.demo.api.realestate.domain.PropertyType
 import com.example.demo.api.realestate.domain.QueryDslEntity.qProperty
+import com.example.demo.api.realestate.handler.common.request.QueryDslRequestParser
 import com.fasterxml.jackson.annotation.JsonValue
+
 import com.querydsl.core.types.OrderSpecifier
 import com.querydsl.core.types.dsl.BooleanExpression
 import io.swagger.annotations.ApiModel
+import java.time.Instant
+import java.util.*
 import javax.validation.constraints.Max
 import javax.validation.constraints.Min
 import com.example.demo.api.realestate.handler.common.request.QueryDslOperation as Op
 
 typealias Filters = List<FieldAndValues>
 typealias Sorting = List<SortField>
+
+private object FieldNames {
+    const val id = "id"
+    const val createdAt = "createdAt"
+    const val modifiedAt = "modifiedAt"
+    const val name = "name"
+    const val type = "type"
+    const val address_country = "address.country"
+    const val address_city = "address.city"
+    const val address_zip = "address.zip"
+    const val address_street = "address.street"
+    const val address_number = "address.street"
+    const val address_district = "address.number"
+    const val address_neighborhood = "address.neighborhood"
+}
 
 data class SearchPropertiesRequest(
         @get:[Min(0)] val offset: Long = 0,
@@ -19,14 +39,6 @@ data class SearchPropertiesRequest(
         val search: Filters? = null,
         val orderBy: Sorting? = null
 )
-
-private object FieldName {
-    const val id = "id"
-    const val name = "name"
-    const val type = "type"
-    const val address_country = "address.country"
-    const val address_city = "address.city"
-}
 
 @ApiModel("SearchPropertiesRequest.FieldAndValues")
 data class FieldAndValues(val field: FilterField, val values: List<String>)
@@ -38,9 +50,17 @@ enum class SortField(
         fieldOperation: String,
         val orderSpecifierSupplier: () -> OrderSpecifier<*>
 ) {
+    modifiedAt_asc(FieldNames.modifiedAt, Op.ASC, { qProperty.modified.asc() }),
+    modifiedAt_desc(FieldNames.modifiedAt, Op.DESC, { qProperty.modified.desc() }),
 
-    name_asc(FieldName.name, Op.ASC, { qProperty.name.asc() }),
-    name_desc(FieldName.name, Op.DESC, { qProperty.name.desc() }),
+    createdAt_asc(FieldNames.createdAt, Op.ASC, { qProperty.created.asc() }),
+    createdAt_desc(FieldNames.createdAt, Op.DESC, { qProperty.created.desc() }),
+
+    name_asc(FieldNames.name, Op.ASC, { qProperty.name.asc() }),
+    name_desc(FieldNames.name, Op.DESC, { qProperty.name.desc() }),
+
+    type_asc(FieldNames.type, Op.ASC, { qProperty.type.asc() }),
+    type_desc(FieldNames.type, Op.DESC, { qProperty.type.desc() }),
 
 
     ;
@@ -62,22 +82,73 @@ enum class FilterField(
         fieldOperation: String,
         val predicateSupplier: (FilterField, String) -> BooleanExpression
 ) {
-    name_like(FieldName.name, Op.LIKE, {
-        _, value: String ->
-        qProperty.name.like(value)
+
+    modifiedAt_goe(FieldNames.modifiedAt, Op.GOE, {
+        field, value: String ->
+        qProperty.modified.goe(value.toInstant(field))
+    }),
+    modifiedAt_loe(FieldNames.modifiedAt, Op.GOE, {
+        field, value: String ->
+        qProperty.modified.loe(value.toInstant(field))
     }),
 
+    createdAt_goe(FieldNames.createdAt, Op.GOE, {
+        field, value: String ->
+        qProperty.created.goe(value.toInstant(field))
+    }),
+    createdAt_loe(FieldNames.createdAt, Op.GOE, {
+        field, value: String ->
+        qProperty.created.loe(value.toInstant(field))
+    }),
+
+    name_like(FieldNames.name, Op.LIKE, {
+        _, value: String ->
+        qProperty.name.likeIgnoreCase(value)
+    }),
+
+    id_eq(FieldNames.id, Op.LIKE, {
+        field, value: String ->
+        qProperty.id.eq(value.toUUID(field))
+    }),
+
+    type_eq(FieldNames.type, Op.LIKE, {
+        field, value: String ->
+        qProperty.type.eq(value.toPropertyType(field))
+    }),
+
+    address_country_like(FieldNames.address_country, Op.LIKE, {
+        _, value: String ->
+        qProperty.address.country.likeIgnoreCase(value)
+    }),
+    address_city_like(FieldNames.address_city, Op.LIKE, {
+        _, value: String ->
+        qProperty.address.city.likeIgnoreCase(value)
+    }),
+    address_zip_like(FieldNames.address_zip, Op.LIKE, {
+        _, value: String ->
+        qProperty.address.zip.likeIgnoreCase(value)
+    }),
+    address_street_like(FieldNames.address_street, Op.LIKE, {
+        _, value: String ->
+        qProperty.address.street.likeIgnoreCase(value)
+    }),
+    address_number_like(FieldNames.address_number, Op.LIKE, {
+        _, value: String ->
+        qProperty.address.number.likeIgnoreCase(value)
+    }),
+    address_district_like(FieldNames.address_district, Op.LIKE, {
+        _, value: String ->
+        qProperty.address.district.likeIgnoreCase(value)
+    }),
+    address_neighborhood_like(FieldNames.address_neighborhood, Op.LIKE, {
+        _, value: String ->
+        qProperty.address.neighborhood.likeIgnoreCase(value)
+    }),
     ;
 
     @get:JsonValue val fieldExpression: String = "$fieldName-$fieldOperation"
 
-    override fun toString(): String {
-        return fieldExpression
-    }
-
-    fun toBooleanExpression(value: String): BooleanExpression {
-        return predicateSupplier(this, value)
-    }
+    override fun toString(): String = fieldExpression
 
     fun toBooleanExpression(values: List<String>): List<BooleanExpression> {
         return values.map { value -> predicateSupplier(this, value) }.toList()
@@ -95,4 +166,20 @@ fun Sorting?.toOrderByExpressionDsl(): List<OrderSpecifier<*>> {
     return this?.map {
         it.toOrderSpecifier()
     } ?: emptyList()
+}
+
+private fun String.toInstant(field: FilterField): Instant {
+    return QueryDslRequestParser.asInstant(
+            fieldValue = this, fieldExpression = field.fieldExpression
+    )
+}
+private fun String.toUUID(field: FilterField): UUID {
+    return QueryDslRequestParser.asUUID(
+            fieldValue = this, fieldExpression = field.fieldExpression
+    )
+}
+private fun String.toPropertyType(field: FilterField): PropertyType {
+    return QueryDslRequestParser.asPropertyType(
+            fieldValue = this, fieldExpression = field.fieldExpression
+    )
 }
